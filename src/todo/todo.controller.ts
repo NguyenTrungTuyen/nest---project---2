15c5c95997a483e396c9462103b91e4d5d489441
NguyenTrungTuyen <tn1481482004@gmail.com>
2025-06-17 13:32:22 +0700

    Update todo.controller.ts

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,41 +1,52 @@
-import { Controller, Get, Post, Put, Delete, Param, Body, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+  Query,
+} from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { Todo } from './todo.schema';
 
 @Controller('todos')
 export class TodoController {
-    constructor(private readonly todoService: TodoService) { }
-    @Get()
-    async findAll(): Promise<Todo[]> {
-        return this.todoService.findAll();
-    }
-    
-    @Get(':id')
-    async findOne(@Param('id') id: string) {
-        return this.todoService.findOne(id);
-    }
+  constructor(private readonly todoService: TodoService) {}
+  @Get()
+  async findAll(): Promise<Todo[]> {
+    return this.todoService.findAll();
+  }
 
-       @Get('search')
-    async search(@Query('status') status: string, @Query('title') title: string) {
-        const boolStatus = status !== undefined ? status === "true" : undefined;
-        return this.todoService.search(boolStatus, title);
-    }
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.todoService.findOne(id);
+  }
 
-    @Post()
-    async create(@Body() body: { title: string; description?: string }): Promise<Todo> {
-        return this.todoService.create(body.title, body.description);
-    }
+  @Get('search')
+  async search(@Query('status') status: string, @Query('title') title: string) {
+    const boolStatus = status !== undefined ? status === 'true' : undefined;
+    return this.todoService.search(boolStatus, title);
+  }
 
-    @Put(':id')
-    async update(
-        @Param('id') id: string,
-        @Body() body: { title: string; completed: boolean },
-    ) {
-        return this.todoService.update(id, body);
-    }
+  @Post()
+  async create(
+    @Body() body: { title: string; description?: string },
+  ): Promise<Todo> {
+    return this.todoService.create(body.title, body.description);
+  }
 
-    @Delete(':id')
-    async delete(@Param('id') id: string) {
-        return this.todoService.delete(id);
-    }
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() body: { title: string; completed: boolean },
+  ) {
+    return this.todoService.update(id, body);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.todoService.delete(id);
+  }
 }
