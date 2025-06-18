34af9b1fe634a23f76f8ec766337ac79c8f61bd0
NguyenTrungTuyen <tn1481482004@gmail.com>
2025-06-18 10:10:16 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,9 @@ import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
-     MongooseModule.forRoot('mongodb://localhost:27017/todo'),
+    //  MongooseModule.forRoot('mongodb://localhost:27017/todo'),
+     ConfigModule.forRoot({ isGlobal: true }), 
+     MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/todos'),
      TodoModule,
   ],
   controllers: [AppController],
